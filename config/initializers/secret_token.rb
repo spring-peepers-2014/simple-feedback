4bebc76a59cf3b780b288da46d5e31c2ce159683
Steven G. Harms <steven@devbootcamp.com>
2014-06-03 11:10:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleFeedback::Application.config.secret_token = 'ce29a87ff240990809f63727156f0b8983648db9fcc1a30ba29b2e4a9a030569e6a4d948e8f1275c0cd09e2775b95a3b7eb046c7f7e87657be01feadb2b4217d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleFeedback::Application.config.session_store :cookie_store, key: '_simple_feedback_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SimpleFeedback::Application.config.session_store :active_record_store
