7fb28a77e4f718e1155bbdc9654f2104a6147bf4
Steven G. Harms <steven@devbootcamp.com>
2014-06-04 12:58:08 -0400

    Remove Feedbacks#show

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -10,14 +10,9 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.create(params[:feedback])
     if @feedback.save
       flash[:success] = "Your feedback has been logged"
-      redirect_to feedback_path(@feedback)
     else
       flash[:alert] = @feedback.errors.full_messages
-      redirect_to feedbacks_path
     end
-  end
-
-  def show
-    @feedback = Feedback.find(params[:id])
+    redirect_to feedbacks_path
   end
 end
