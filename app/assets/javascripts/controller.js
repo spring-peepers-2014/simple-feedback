814d53bd36801960e60ca6ecc551afbdaed46249
Lawrence Manfredi <lrmanfre@me.com>
2014-06-05 16:21:27 -0400

    gets raw text, checks for asterisks with for loop

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -2,11 +2,35 @@ $( document ).ready(function() {
 
 
 
+var form_body = $("#new_feedback");
+var body_textarea = $("textarea#feedback_body");
+var text_to_convert;
 
-$("#new_feedback").keyup(function(e) {
-  var input = $("textarea#feedback_body").val();
-  console.log("input: " + input);
+
+
+$(form_body).keyup(function(e) {
+  var text = getRawText();
+  markdownConverter(text);
 });
+
+var getRawText = function() {
+  return $(body_textarea).val();
+}
+
+var markdownConverter = function(text) {
+  console.log("text: " + text);
+
+  checkForBold(text);
+}
+
+var checkForBold = function(text) {
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] === "*") {
+      console.log("HALLELUJAH");
+    }
+
+  }
+}
 
 // var getRawText = function() {
 //   var input = $('')
