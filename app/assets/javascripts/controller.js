a04ab49d377ea10579e5b8fbacdcfe3a139ec306
Lawrence Manfredi <lrmanfre@me.com>
2014-06-05 15:55:24 -0400

    get raw text in js

---
+++ app/assets/javascripts/controller.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+
+
+
+$("#new_feedback").keyup(function(e) {
+  var input = $("textarea#feedback_body").val();
+  console.log("input: " + input);
+});
+
+// var getRawText = function() {
+//   var input = $('')
+// }
+
+
+
+//  {
+
+// getRawText = function
+
+// var textToAppend = ConvertToMarkdown.new(rawText)
+//                     -convertTomdown()
+
+// updatePreview = function
+
+});
